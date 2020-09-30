085c73b157e153a76505cd32470d429630be1734
bocephus <itx2@outlook.com>
2020-09-30 10:24:37 -0400

    Add files via upload

---
+++ backup.ps1
@@ -0,0 +1,7 @@
+﻿$username = read-host "What is the users 'username' "
+
+$compHost = hostname
+
+$destination = read-host "Where would you like to save the backup? "
+
+copy-item C:\Users\$username\Documents\*.* -Recurse $destination -Verbose
